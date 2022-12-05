d09c7fa7fadcc910e6ab9c3e29d35a3c08fec2f1
Zihan-Xu527 <zxu64@ucmerced.edu>
2022-12-05 00:19:09 -0800

    project refactor

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void writeResultToFile(std::vector<double> & vec1, std::vector<double> & vec2, s
 {
     std::ofstream output;
     int n = vec1.size();
-    std::string fileName = "../sample_size";
+    std::string fileName = "../output/sample_size";
     fileName.append("_").append(std::to_string(n)).append(".csv");
     output.open(fileName);
     for (int i = 0; i < n; i++)//save stock price and hedge payoff into a csv file
@@ -25,9 +25,75 @@ void writeResultToFile(std::vector<double> & vec1, std::vector<double> & vec2, s
 
 }
 
+void check_reduction(double r, double sigma, double S_0, double T, double K){
+
+    std::vector<int> nn = {10, 100, 1000, 10000};
+//    omp_set_num_threads(8);
+    int m = 10000;
+    std::vector<double> Y_bar(m);
+    std::vector<double> Yb_bar(m);
+
+
+    for (int i = 0; i < nn.size(); i++){
+        std::cout << "n = " << nn[i] <<", ";
+#pragma omp parallel for
+        for(int j = 0; j < m; j++) {
+            controlVariates VR(r, sigma, S_0, T, K, nn[i]);
+
+            VR.init();
+            VR.exec();
+
+            Yb_bar[j] = average(VR.get_sol());
+            Y_bar[j] = average(VR.get_payoff());
+        }
+
+        double var_Y = variance(Y_bar), var_Yb = variance(Yb_bar);
+        double rho_squared = 1-var_Yb/var_Y;
+        std::cout << "variance of Y is " << var_Y << ", ";
+        std::cout <<"variance of Y(b) is "<< var_Yb << ", ";
+        std::cout <<"rho^2 is " << rho_squared << std::endl;
+
+    }
+}
+
+void check_asymptotic_result(double r, double sigma, double S_0, double T, double K){
+
+    std::vector<int> nn = {10, 100, 1000, 10000};
+    double EY = expected_Payoff(0, S_0, r, sigma, K, T);
+    std::cout<<"E[Y] = "<< EY << std::endl;
+    int m = 10000;
+
+
+    for(int i = 0; i < nn.size(); i++){
+
+        int n = nn[i];
+        std::cout << "n = "<< n <<", ";
+        int count = 0;
+
+#pragma omp parallel for
+        for(int j = 0; j < m; j++) {
+            std::vector<double> spot_maturity(n);
+            std::vector<double> discounted_payoff(n);
+
+            controlVariates VR(r, sigma, S_0, T, K, nn[i]);
+
+            VR.init();
+            VR.exec();
+
+            std::vector<double> interval = confidenceInterval(VR.get_sol());
+            if( interval[0] < EY && EY < interval[1])
+            {
+                count += 1;
+            }
+        }
+
+        std::cout << "the probability that E[Y] lies in the 95% confidence interval is " << count / (m + 0.)<< std::endl;
+
+    }
+}
 int main() {
-    void check_reduction(double r, double sigma, double S_0, double T, double K);
-    void check_asymptotic_result(double r, double sigma, double S_0, double T, double K);
+
+//    void check_asymptotic_result(double r, double sigma, double S_0, double T, double K);
     double r = 0.05, sigma = 0.3;
     double S_0 = 50.0, T = 0.25, K = 55.;
     double S_T = S_0 * std::exp( r * T );
